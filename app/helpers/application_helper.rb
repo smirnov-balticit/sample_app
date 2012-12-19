d8dbf940e32bac92eb2148d8d3afe40e1821b7f3
anstak <anstak@gmail.com>
2012-12-19 21:57:04 +0300

    chapter 5 final end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 module ApplicationHelper
+  def title
+    base_title = "ROR"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
 
+  end
+  def logo
+  	return image_tag("logo.png", :alt => "Sample App1", :class => "round")
+  end
+  
+  
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,3 @@
 module PagesHelper
-  def title
-    base_title = "ROR"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-
-  end
 
 end
