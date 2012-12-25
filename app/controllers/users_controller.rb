c62e14aa333eb55d2951edd5c51dd3829070f204
anstak <anstak@gmail.com>
2012-12-25 23:21:13 +0300

    Chapter 8 end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,21 @@ class UsersController < ApplicationController
 		@title = @user.name
 	end
  
-  def new
-  	@title = "Sign up"
-  end
+	def new
+		@user = User.new
+		@title = "Sign up"
+	end
+	
+	def create
+		@user = User.new(params[:user]) #params[:user] эквивалент name:"Foo Bar",email:"foo@invalid", password:"dude", password_confirmation:"dude"
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			@title = "Sign up"
+			render 'new'
+		end
+	end
+	
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
-
+  
+ 
 class User < ActiveRecord::Base
-	attr_accessor :password 
+	attr_accessor :password  
  	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i	 
